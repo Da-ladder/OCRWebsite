61412dbef6da0b6fd762eb6c0c80ba54853fd9b7
Da-ladder <115005260+Da-ladder@users.noreply.github.com>
2023-12-08 08:07:49 -0500

    django html test

--- helloWorld/polls/urls.py
+++ helloWorld/polls/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    # ex: /polls/tester/
+    path("tester/", views.test, name="TEST"),
 ]
