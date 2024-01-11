5bfa8e377d503a813b36b082197b4d1c2da74a3f
potato <zcody007@gmail.com>
2024-01-10 22:04:17 -0500

    1/10/2024 dir removed

--- helloWorld/helloWorld/urls.py
+++ helloWorld/helloWorld/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
     path("", include("websiteFace.urls")),
     path("admin/", admin.site.urls),
 ]
