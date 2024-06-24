251a57de02aeac8d7c03a9568ad6b59f2d326f14
potato <zcody007@gmail.com>
2024-06-24 14:54:07 -0400

    still fixing data

--- helloWorld/websiteFace/models.py
+++ helloWorld/websiteFace/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class VideoStorage(models.Model):
-    name = models.CharField(max_length=20, blank=True, null=True)
     vid_name = models.CharField(max_length=100)
     vid_sec_length = models.BigIntegerField()
     vid_key = models.CharField(max_length=11, null=True)
