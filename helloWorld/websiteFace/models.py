707e1d1dccbf988a03c299fbc192ab2022e62e8f
grindyyyy <grindyyyyyyyyyy@gmail.com>
2024-06-24 15:02:23 -0400

    Sigma 2 Lines of code

--- helloWorld/websiteFace/models.py
+++ helloWorld/websiteFace/models.py
@@ -6,6 +6,8 @@ class VideoStorage(models.Model):
     vid_key = models.CharField(max_length=11, null=True)
     vid_extracted = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.vid_name
 
 
 # Create your models here.
