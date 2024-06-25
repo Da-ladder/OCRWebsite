56cdc72cc7a728daa42749f7379db8b55103e65f
grindyyyy <grindyyyyyyyyyy@gmail.com>
2024-06-24 20:42:37 -0400

    Sigma

--- helloWorld/websiteFace/tasks.py
+++ helloWorld/websiteFace/tasks.py
@@ -1,2 +1,10 @@
-from .webFuncts.find import VideoAnalysis
+from celery import Celery
+import time
 
+app = Celery('tasks', broker='amqp://guest:guest@localhost:5672//', backend="rpc://")
+
+@app.task
+def a():
+    time.sleep(10)
+    return "!!!!!!!!!"
+    
