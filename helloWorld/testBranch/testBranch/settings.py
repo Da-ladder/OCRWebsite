2bd9312ef6b4946e1da1ce2ae8cb66da1fdde41f
Da-ladder <zcody007@gmail.com>
2024-07-12 16:49:27 -0400

    CSRF admin failure fixed - cod

--- helloWorld/testBranch/testBranch/settings.py
+++ helloWorld/testBranch/testBranch/settings.py
@@ -22,14 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-8^q3js-217(_4to#r5_uc-vpe%^m3c329%f#vk96zrxep7-i^n'
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'khyuge*7834))$($(*@$893))'#os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # will not serve static files automatically without debug
 #DEBUG = True
-DEBUG = os.getenv('DEBUG')
+DEBUG = True#os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ["127.0.0.1","*","localhost", "73.68.207.64"]
+ALLOWED_HOSTS = ["http://127.0.0.1","*","localhost", "73.68.207.64", "192.168.1.230"]
+CSRF_TRUSTED_ORIGINS = ['http://dhsclubs.org', 'http://192.168.1.230']
 
 
 
@@ -65,7 +66,10 @@ SOCIALACCOUNT_PROVIDERS = {
         "AUTH_PARAMS": {"access_type": "online", 
                         "include_granted_scopes": "true",
                         },
-        "VERIFIED_EMAIL": True
+        "VERIFIED_EMAIL": True,
+        'OAUTH2_PARAMS': {
+            'redirect_uri': 'http://django.com/accounts/google/login/callback/',
+        }
     }
 }
 
@@ -165,6 +169,6 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 
-ACCOUNT_ADAPTER = 'users.authConfig.MyAccountAdapter'
-LOGIN_REDIRECT_URL = "/clubs"
+#ACCOUNT_ADAPTER = 'users.authConfig.MyAccountAdapter'
+LOGIN_REDIRECT_URL = "/clubs/"
 LOGOUT_REDIRECT_URL = "/"
