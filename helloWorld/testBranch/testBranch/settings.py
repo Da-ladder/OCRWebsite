e85437f9dd5299a91f98222b869c38c394278309
Da-ladder <zcody007@gmail.com>
2024-08-06 15:54:21 -0400

    favicon & club screen edits - cod

--- helloWorld/testBranch/testBranch/settings.py
+++ helloWorld/testBranch/testBranch/settings.py
@@ -19,16 +19,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+devMode = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+if not devMode:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# will not serve static files automatically without debug
-if os.getenv('DEBUG') == "false":
-    DEBUG = False
+    # SECURITY WARNING: don't run with debug turned on in production!
+    # will not serve static files automatically without debug
+    if os.getenv('DEBUG') == "false":
+        DEBUG = False
+    else:
+        DEBUG = True
 else:
     DEBUG = True
+    SECRET_KEY = "development-key-exposedEUkm398278sunL98e89"
 
 ALLOWED_HOSTS = ["https://dhsclubs.org", "https://www.dhsclubs.org", "http://127.0.0.1", "http://dhsclubs.org", "http://www.dhsclubs.org", "*"]
 CSRF_TRUSTED_ORIGINS = ["https://dhsclubs.org", "https://www.dhsclubs.org", "http://127.0.0.1", "http://dhsclubs.org", "http://www.dhsclubs.org"]
--- helloWorld/testBranch/testBranch/urls.py
+++ helloWorld/testBranch/testBranch/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from . import views
 from django.contrib import admin
 from django.urls import path, include
@@ -22,5 +24,6 @@ urlpatterns = [
     #path("", views.frontPage, name="TEST"),
     path('admin/', admin.site.urls),
     path("accounts/google/login", include("allauth.urls")),
-    path("", include("users.urls"))
+    path('favicon.ico', RedirectView.as_view(url=('/static/favicon.ico'))),
+    path("", include("users.urls")),
 ]
