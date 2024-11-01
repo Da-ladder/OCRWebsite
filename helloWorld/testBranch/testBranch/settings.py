efc8a263e944b5e9d8e0a9a8b08aa7b4d61ef67e
TimePixel45 <148555740+TimePixel45@users.noreply.github.com>
2024-11-01 14:47:42 -0400

    Updated clubs page

--- helloWorld/testBranch/testBranch/settings.py
+++ helloWorld/testBranch/testBranch/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-devMode = False
+devMode = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
