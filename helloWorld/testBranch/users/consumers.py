0719b99a6dd323156fa43dda70322ce380cf5707
Da-ladder <zcody007@gmail.com>
2025-03-28 14:37:23 -0400

    added things to requirements.txt & master control of rounds

--- helloWorld/testBranch/users/consumers.py
+++ helloWorld/testBranch/users/consumers.py
@@ -3,6 +3,8 @@ import json
 import time # testing delay w/ async
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
+from asgiref.sync import async_to_sync
 from channels.db import database_sync_to_async
 
 # imports all databases
@@ -29,8 +31,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"].strip()
+        userOptPicked = int(text_data_json["optionSelected"])
 
-        if (len(message) == 0):
+        # if branch when 
+        if (userOptPicked != -1):
+            self.checkAnswer()
+
+            return
+        elif (len(message) == 0):
             return # makes sure the message is not just spaces
     
         await self.post_comment(message)
@@ -45,17 +53,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
      [Option 1 True/False, Option 2 True/False, - Question Answers
      Option 3 True/False, Option 4 True/False], - Question Answers
      Countdown? True/False, - gives the message to start countdown
-     [Question, Option 1, Option 2, Option 3], - Array of Questions (OVERRIDES COUNTDOWN WHEN SENT)
+     [Question, Option 1, Option 2, Option 3, Option 4], - Array of Questions (OVERRIDES COUNTDOWN WHEN SENT)
      eliminated? True/False, ] - State of the user, if they are eliminated, answers will NOT be
                                  sent to the server.
     """
     # Receive message from room group
     # triggers for every connected websocket when ".group_send" is called
-    async def chat_message(self, event):
+    async def chat_message(self, event):    
         message = event["message"]
     
         # Send message to WebSocket - eliminated parameter is a one way switch so we send False as a default
         await self.send(text_data=json.dumps([message, [], False, [], False]))
+
+    async def start_countdown(self, event):
+        await self.send(text_data=json.dumps(["", [], True, [], False]))
+
+    async def send_questions(self, event):
+        question = event["question"]
+        opt1 = event["opt1"]
+        opt2 = event["opt2"]
+        opt3 = event["opt3"]
+        opt4 = event["opt4"]
+        await self.send(text_data=json.dumps(["", [], False, [question, opt1, opt2, opt3, opt4], False]))
 
     # Log user comments
     @database_sync_to_async
@@ -84,15 +103,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # it will currently fetch from the db EVERY time it needs to check the answer,
     # improve this via using cache & redis later on
     @database_sync_to_async
-    def checkAnswer(answer):
+    def checkAnswer(self, answer):
         club_wrapper = Club.objects.get(name = "Trivia")
 
         # Get the question list
         questionList = ClubData.objects.filter(club=club_wrapper).latest('creationTime')
-        questionList = questionList.data()
+        questionList = questionList.data
 
         # get the current question that we are on
+        if (not questionList[1]):
+            return False
         curQnum = questionList[0]
+        
 
         # answer*2+2 is the exact index we need to fetch the answer from to compare it to the user's answer
         # the if statement will evaluate to true if the answer is correct
@@ -101,11 +123,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         else:
             # remove them from the "player list"
             pass
-        
-        pass
 
 
 class AdminConsumer(AsyncWebsocketConsumer):
+
     async def connect(self):
         self.room_group_name = f"_triviaAdmin" # sets all admin websockets to link together 'room_triviaAdmin'
 
@@ -118,16 +139,65 @@ class AdminConsumer(AsyncWebsocketConsumer):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    # Receive message from WebSocket
+    # Receive message from WebSocket dkdj
     async def receive(self, text_data):
-        pass
+        if (text_data == "START"):
+            await self.startRound()
+            
+        sync_to_async(time.sleep(6))
+
 
     # Receive message from room group
     async def chat_message(self, event):
         pass
-    
-    # it will currently fetch from the db EVERY time it needs to check the answer,
-    # improve this via using cache & redis later on
+
     @database_sync_to_async
-    def startRound():
-        pass
+    def startRound(self):
+        club_wrapper = Club.objects.get(name = "Trivia")
+
+        # Get the question list
+        dBquestionList = ClubData.objects.filter(club=club_wrapper).latest('creationTime')
+        questionList = json.loads(dBquestionList.data)
+
+        # Abort if another round is in progress
+        if (questionList[1]):
+            print("Aborted round: round in progress!")
+            return
+        
+        # locks down round
+        # questionList[1] = True # blocks another round from starting
+        dBquestionList.data = json.dumps(questionList)
+        dBquestionList.save() 
+
+        curQuest = questionList[0]
+        
+        # start countdown for all users
+        print("Countdown Started")
+        async_to_sync(self.channel_layer.group_send)(
+            "_trivia", {"type": "start.countdown"} # . is replaced with _ so chat.message calls chat_message
+        )
+        
+        # wait for countdown to end
+        time.sleep(4)
+
+
+        print("Current Question: " + str(curQuest))
+
+        # get questions from the database to give to the user
+        # index 2 is where the question list is and the other numbers are due to
+        # dB storage being opt1, t/f ans, opt2, t/f ans...
+        async_to_sync(self.channel_layer.group_send)(
+            "_trivia", {"type": "send.questions", "question" : questionList[2][curQuest][0], 
+                        "opt1" : questionList[2][curQuest][1],
+                        "opt2" : questionList[2][curQuest][3],
+                        "opt3" : questionList[2][curQuest][5],
+                        "opt4" : questionList[2][curQuest][7]}
+        )
+        print("Questions and options given")
+
+
+        # time.sleep(31) # 30 seconds to answer the question + some headroom
+
+        # shut down ability to answer questions
+
+        print("Round over")
